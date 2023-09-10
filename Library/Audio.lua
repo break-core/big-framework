8d9575278f666712c17a837523feb440853dda90
riall <71668864+break-core@users.noreply.github.com>
2023-09-10 18:31:18 -0400

    Audio + Player 100% line match

---
+++ Library/Audio.lua
@@ -0,0 +1,439 @@
+--[[
+	                                                                ,,╓╓╥╗╗@@@╣╢
+	                                               ,²╓╓╥╗@@@╣╣╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒U
+	                               ,,╓╓╥m╗@@@╣╢▒▒▒▒▒▒▒▒▒Ñ╜`         "╙╣▒▒▒▒▒▒▒▒╣
+	               ,,╓╓╗╗╗@@╣╣╢▒▒▒▒▒▒╣╝╨╜╙"`║▒▒▒▒▒▒▒▒╢╜                `╣▒▒▒▒▒▒▒
+	,╓╓╥╗@@@╣╢╢▒▒▒▒ÑÑ╝╝╨╜╨╝╝╣▒▒▒▒▒▒▒▒╢      ]▒▒▒▒▒▒▒╝         ,,         ╙▒▒▒▒▒▒
+	║▒▒▒▒▒"`                  `╝▒▒▒▒▒▒       ▒▒▒▒▒▒╝      ,@▒▒▒▒▒╣╗     ╓╗╣▒▒▒▒▒[
+	 ▒▒▒▒▒U                     └▒▒▒▒▒       ║▒▒▒▒╣       ╢▒▒▒▒▒▒▒▒╢╗╣╣▒▒▒▒▒▒▒▒▒▒
+	 ▒▒▒▒▒╣       ²╓╓╥╗╗╗╖       ]▒▒▒▒[      ]▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+	 ║▒▒▒▒▒       ▒▒▒▒▒▒▒▒╣      ]▒▒▒▒▒       ▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒╢╢Ñ╝╨╜╙╙▒▒▒▒▒[
+	 ]▒▒▒▒▒       ╢▒▒▒▒▒▒╢╜      ║▒▒▒▒▒       ╢▒▒▒[      ]▒▒▒▒▒[            ╢▒▒▒▒╢
+	  ▒▒▒▒▒[      └╙"``         @▒▒▒▒▒▒[      ]▒▒▒╢       ▒▒▒▒▒╣            ║▒▒▒▒▒
+	  ║▒▒▒▒╢                   ²╙╣▒▒▒▒▒▒       ▒▒▒▒       ║▒▒▒▒▒   ,²╓      ]▒▒▒▒▒U
+	  ]▒▒▒▒▒                       ╣▒▒▒▒       ╢▒▒▒╣       ╢▒▒▒▒▒▒▒▒▒╜       ▒▒▒▒▒╣
+	   ▒▒▒▒▒[      ]@╣╣▒▒▒▒╣╗       ▒▒▒▒[      ║▒▒▒▒╗       ╙╣▒▒▒▒▒Ñ`        ║▒▒▒▒▒
+	   ╢▒▒▒▒╢       ▒▒▒▒▒▒▒▒▒U      ║▒▒▒╢      ]▒▒▒▒▒@                       ]▒▒▒▒▒
+	   ║▒▒▒▒▒       ╣▒▒▒▒▒▒▒╢       ║▒▒▒▒       ▒▒▒▒▒▒╣╖               ,[     ▒▒▒▒▒[
+	    ▒▒▒▒▒U      ║Ñ╝╨╜╙"²        ▒▒▒▒▒       ║▒▒▒▒▒▒▒▒@╖,        ,╥╣▒╢@@╣╣╢▒▒▒▒╢╢
+	    ▒▒▒▒▒[                    ╓╢▒▒▒▒▒[      ]▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢╢▒╢ÑÑ╝╝╜╙""`
+	    ║▒▒▒▒▒                ,╓@╢▒▒▒▒▒▒▒▒@╣╣╢▒▒▒▒╢Ñ╝╝╨╜╙""`           ╓╗@@@╗╖
+	    ]▒▒▒▒▒    ,,╓╓╗╗@@@╣▒▒▒▒▒▒╢ÑÑ╝╝╜╙"``        ,²╓  ╔@@@╣╣╢▒▒▒U ,╣▒Ñ╙"╙╢▒Ñ
+	     ▒▒▒▒▒▒▒▒▒╢Ñ╝╝╨╜╙"``             ╥@@@╗    ]▒▒▒▒  ]▒▒╣"``     ]▒▒╗,
+	     "``       ,╓╖╓,       ]▒▒▒╣     ]▒▒▒▒╖   ║▒▒▒▒[  ▒▒▒   ,,    ╙╣▒▒▒▒╣%╖
+	            ,╣▒▒╢╣╣▒▒@     ╢▒▒╣▒╣     ▒▒╢╣▒╖  ▒▒[▒▒╢  ║▒▒▒▒▒▒▒r       `"╙╣▒▒╕
+	            ╣▒╣    ╙╜`     ▒▒╢ ╣▒╣    ▒▒▒╙▒▒╕]▒▒ ║▒▒  ]▒▒[         ╓@     ▒▒╢
+	           ║▒▒[       ,   ]▒▒[  ▒▒╣   ║▒▒ ╙▒▒@▒▒ ]▒▒L  ▒▒╢   ,,╓╓  ╣▒▒╣@╣▒▒╣
+	           ]▒▒[  ]╢▒▒▒▒[  ║▒▒╣╣╣╢▒▒╣  ]▒▒[ ╙▒▒▒[  ▒▒╣  ╢▒▒▒▒▒▒▒╢Ñ~    "╙"`  		
+	            ╢▒▒     ]▒▒╢  ╢▒▒╙"``╙▒▒@  ▒▒╢  ╙▒╢`  ╨╜╜  ``
+	             ╣▒▒h╖╥@▒▒▒▒ j▒▒╣     ╙╜╙` `
+	              `╙╝ÑÑ╝╙ "`
+                                                       
+    
+	BIG Games rbx.lua Framework [2017] - [2023]
+	Written by Preston - preston@biggames.io
+	Developed with a keyboard and pixie dust.
+	
+	-+-+-+- SPECIFICS -+-+-+-
+	_L.Audio
+	===========
+	Play SFX and Music through a single module. Pretty basic stuff.
+	You can cross-fade music tracks or edit all sound entities without using soundgroups - useful for lots of rapid fire sounds that
+	would normally cause sound tearing. Bundled with tons of little random QOL features. Built-in garbage collection, too :D
+	===========
+		\\\ Play SFX
+		Audio.Play(
+			id / ids,				<-- |REQ|	Sound ID; Supports link or just ID. (also supports array of IDs. Will pick one by random)
+			parent / position,		<-- |REQ|	Sound parent or position (cf/v3)
+			pitch,					<--			Sound pitch 										[defaults to: 1]
+			volume,					<-- 		Sound volume										[defaults to: 0.5]
+			max distance,			<-- 		Sound max distance in studs 						[defaults to: 100]
+			sound group,			<-- 		Sound group											[defaults to: nil]
+			looped,					<-- 		Sound looped 										[defaults to: false]
+			start time				<-- 		Sound start time in seconds 						[defaults to: 0]
+		)
+		
+		\\\ Play background music or ambience. Will automatically crossfade with any song that is already playing.
+		Audio.PlayMusic(
+			id,						<-- |REQ|	Music ID (without link)
+			volume,					<-- 		Music volume										[defaults to: 1]
+			speed,					<-- 		Crossfade speed in seconds							[defaults to: 1]
+			sound group,			<-- 		Sound group											[defaults to: nil]
+			looped,					<-- 		Music looped 										[defaults to: true]
+			start time				<-- 		Music start time in seconds 						[defaults to: 0]
+		)
+		
+		\\\ Stops any song that is currently playing.
+		Audio.StopMusic(
+			speed,					<-- 		Crossfade speed in seconds							[defaults to: 1]
+		) 
+	===========
+--]]
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--------|       Top       |--------
+local Audio = {};
+
+--------|     Setting     |--------
+
+--------|     Library     |--------
+local ReplicatedStorage = game:GetService("ReplicatedStorage");
+local Library = ReplicatedStorage:WaitForChild("Library");
+local ClientLibrary = Library:WaitForChild("Client");
+
+--------|     Shared      |--------
+local RunService = game:GetService("RunService");
+local Functions = require(Library.Functions);
+
+--------|    Reference    |--------
+local debris = workspace:WaitForChild("__DEBRIS");
+local musicFolder = Instance.new("Folder", script);
+local rng = Random.new();
+
+--------|    Variables    |--------
+local garbage = {};
+local isClient = RunService:IsClient();
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--[[
+	Roblox audio player...
+	Very customizable! Please read the documentation above
+]]
+function Audio.Play(...) -- Line: 109
+	--- If server, tell clients to play sound (to keep a secular soundscape)
+	if not isClient then
+		local ServerScriptService = game:GetService("ServerScriptService");
+		local ServerNetwork = require(ServerScriptService:WaitForChild("Library").Network);
+		local args = {...};
+		ServerNetwork.FireAll("PlaySound", unpack(args));
+		return nil;
+	end
+	
+	--- Unpack
+	local id, parent, pitch, volume, maxDistance, soundGroup, looped, startTime = unpack({...});
+	
+	--- Array of IDs support
+	if type(id) == "table" then
+		id = id[rng:NextInteger(1, #id)];
+	end
+	
+	--- Sanity checks
+	if not parent then
+		--- Abort! Parent doesn't exist.
+		warn("Parent cannot be nil", debug.traceback());
+		return
+	elseif id == 0 then
+		--- Sound ID is 0 (removed error because console spamming issues)
+		return
+	end
+	
+	--- Missing link
+	if type(id) == "number" or (not string.find(id, "rbxassetid://")) then
+		id = "rbxassetid://" .. id;
+	end
+	
+	--- Array of pitches support
+	if pitch and type(pitch) == "table" then
+		pitch = rng:NextNumber(unpack(pitch));
+	end
+	
+	--- Array of volumes support
+	if volume and type(volume) == "table" then
+		volume = rng:NextNumber(unpack(volume));
+	end
+	
+	--- Variables
+	pitch = pitch or 1;
+	volume = volume or 0.5;
+	soundGroup = soundGroup and game.SoundService:FindFirstChild(soundGroup) or nil;
+	looped = looped or false;
+	maxDistance = maxDistance or 100;
+	startTime = startTime == nil and 0 or startTime;
+	--
+	
+	local rollOffMode = Enum.RollOffMode.Linear;
+	local name = "sound-" .. id;
+	local parentWasGenerated = false;
+	
+	--- If parent is Vector3/CFrame - create sound part!
+	if not pcall(function() local x = parent.Parent end) then
+		local position = parent;
+		
+		--- If Vector3, convert to CFrame
+		pcall(function() -- Line: 169
+			position = CFrame.new(position);
+		end);
+		
+		--- Create host part for sound
+		parent = Instance.new("Part");
+		parent.Anchored = true;
+		parent.CanCollide = false;
+		parent.CFrame = position;
+		parent.Size = Vector3.new();
+		parent.Transparency = 1;
+		parent.Parent = debris;
+		
+		--- Flag parent for garbage collection
+		parentWasGenerated = true;
+	end
+	
+	--- Create
+	local sound = Instance.new("Sound");
+	sound.SoundId = id;
+	sound.Name = name;
+	sound.Pitch = pitch;
+	sound.Volume = volume;
+	sound.SoundGroup = soundGroup;
+	sound.Looped = looped;
+	sound.MaxDistance = maxDistance;
+	sound.TimePosition = startTime;
+	sound.RollOffMode = rollOffMode;
+	sound.Parent = parent;
+	
+	--- Muted?
+	if not require(ClientLibrary.Settings).SoundsEnabled then
+		sound:SetAttribute("CachedVolume", sound.Volume);
+		sound.Volume = 0;
+	end
+	
+	--- Play sound
+	sound:Play();
+	
+	--- Add to garbage collection
+	AddToGarbageCollection(sound, parentWasGenerated);
+	
+	--
+	return sound;
+end
+
+
+function Audio.PlayMusic(id, volume, speed, soundGroup, looped, startTime) -- Line: 217
+	--- Disable being called on server (compatibility reasons)
+	if not isClient then
+		warn("Cannot use this on the server!");
+		return;
+	end
+	
+	--- Sanity checks
+	if not require(ClientLibrary.Settings).MusicEnabled then
+		--- Abort! Sounds are disabled in settings
+		warn("Music is disabled in settings module!");
+		return;
+	end
+	
+	if id == 0 then
+		--- Sound ID is 0
+		return
+	end
+	
+	--- Missing link
+	if type(id) == "number" or not string.find(id, "rbxassetid://", 1, true) then
+		id = "rbxassetid://" .. id;
+	end
+	
+	--- Variables
+	volume = volume or 0.5;
+	speed = speed or 1;
+	soundGroup = soundGroup and game.SoundService:FindFirstChild(soundGroup) or nil;
+	looped = looped == nil and true or looped;
+	startTime = startTime == nil and 0 or startTime;
+	--
+	local name = "music-" .. id;
+	
+	--- Create
+	local music = Instance.new("Sound");
+	music.SoundId = id;
+	music.Volume = 0;
+	music.Looped = looped;
+	music.SoundGroup = soundGroup;
+	music.Parent = musicFolder;
+	music:Play();
+	
+	--- Fade function
+	local function Fade(song, newVolume) -- Line: 260
+		Functions.Tween(song, {Volume = newVolume}, {speed});
+	end
+	
+	--- Fade out
+	for _, song in ipairs(musicFolder:GetChildren()) do
+		Functions.Tween(song, {Volume = 0}, {speed});
+	end
+	
+	--- Fade in
+	Functions.Tween(music, {Volume = volume}, {speed});
+	
+	--
+	AddToGarbageCollection(music);
+	--
+	return music;
+end
+
+
+function Audio.StopMusic(speed) -- Line: 279
+	--- Disable being called on server (compatibility reasons)
+	if not isClient then
+		warn("Cannot use this on the server!");
+		return;
+	end
+	
+	--- Variables
+	speed = speed or 1;
+	
+	--- Fade function
+	local function Fade(song, newVolume) -- Line: 290
+		Functions.Tween(song, {Volume = newVolume}, {speed});
+	end
+	
+	--- Stop current music
+	for _, song in ipairs(musicFolder:GetChildren()) do
+		Functions.Tween(song, {Volume = 0}, {speed});
+	end
+end
+
+
+------------------------------------------------------------------------------------------------------------------------
+
+
+--- Pile onto garbage collection
+function AddToGarbageCollection(sound, parentWasGenerated) -- Line: 305
+	--- Add garbage collection
+	table.insert(garbage, {sound, parentWasGenerated or false});
+end
+
+
+--- Scan garbage collection for GARBAGE
+function ScanGarbageCollection() -- Line: 312
+	for i = #garbage, 1, -1 do
+		if i % 25 == 0 then
+			RunService.Heartbeat:Wait();
+		end
+		
+		--- Variables
+		local garbageArray = garbage[i];
+		local garbageItem = garbageArray[1];
+		local garbageParentWasGenerated = garbageArray[2];
+		local due = false;
+		
+		--- Sanity check
+		if not due and (not garbageItem or not garbageItem.Parent) then
+			due = true;
+		end
+		
+		--- Stopped check #1
+		if not due and not garbageItem.Playing then
+			due = true;
+		end
+		
+		--- Empty sound check
+		if not due and garbageItem.SoundId == "" then
+			due = true;
+		end
+		
+		--- Remove if due
+		if due then
+			if garbageParentWasGenerated then
+				if garbageItem.Parent then
+					Functions.AddDebris(garbageItem.Parent, 0);
+				end
+			elseif garbageItem then
+				Functions.AddDebris(garbageItem, 0);
+			end
+			table.remove(garbage, i);
+		end
+	end
+	
+	--- Debug
+	if isClient then
+		require(ClientLibrary.Debug).Track("Garbage #", "Audio", #garbage);
+	end
+end
+-- coroutine.wrap(function()
+-- 	if isClient then
+-- 		while true do
+-- 			wait(1)
+-- 			ScanGarbageCollection()
+-- 			local musicPlaying = false
+-- 			for _, music in ipairs(musicFolder:GetChildren()) do
+-- 				musicPlaying = true
+-- 				break
+-- 			end
+-- 			_L.Debug.Track("Music Playing", "Audio", musicPlaying)
+-- 		end
+-- 	end
+-- end)()
+coroutine.wrap(function() -- Line: 371
+	if isClient then
+		while true do
+			--- Wait
+			wait(1);
+			
+			--- Scan garbage
+			ScanGarbageCollection();
+			local audioPlaying = false;
+			
+			--- Loop around
+			for _, audio in ipairs(musicFolder:GetChildren()) do
+				audioPlaying = true
+				break
+			end
+			
+			require(ClientLibrary.Debug).Track("Music Playing", "Audio", audioPlaying);
+		end
+	end
+end)()
+
+
+--- Disabled sounds
+if isClient then
+	coroutine.wrap(function() -- Line: 395
+		local Settings = require(ClientLibrary.Settings);
+		local last = Settings.SoundsEnabled;
+		while true do
+			if last ~= Settings.SoundsEnabled then
+				last = Settings.SoundsEnabled;
+				
+				--- Scan all active sounds
+				for i, v in pairs(garbage) do
+					local sound = v[1];
+					if not sound or sound.Parent == musicFolder then
+						if not last then
+							--- Mute sound and cache volume
+							sound:SetAttribute("CachedVolume", sound.Volume);
+							sound.Volume = 0;
+							
+						elseif not sound:GetAttribute("CachedVolume") then
+							-- Make the attribute
+							sound.Volume = sound:GetAttribute("CachedVolume");
+							sound:SetAttribute("CachedVolume", nil);
+						end
+					end
+				end
+			end
+			
+			-- WAIT!!
+			RunService.Heartbeat:Wait();
+		end
+	end);
+end
+
+
+--- Recieve sounds from server
+if isClient then
+	require(ClientLibrary.Network).Fired("PlaySound"):Connect(function(...) -- Line: 429
+		Audio.Play(...);
+	end);
+end
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+return Audio;
---
+++ Library/Player.lua
@@ -0,0 +1,218 @@
+--------|     Library     |--------
+
+--------|    Reference    |--------
+local Players = game:GetService("Players");
+
+--------|     Setting     |--------
+local Player = {
+	Optional = {},
+	Player = function(plrInst) return plrInst or Players.LocalPlayer end
+};
+
+--- Character primitives
+local function Character(plrInst)
+	return Player.Player(plrInst).Character;
+end
+Player.Optional.Character = Character;
+function Player.Character(plrInst)
+	return Player.Player(plrInst).Character or Player.Player(plrInst).CharacterAdded:Wait();
+end
+
+--- Part-based primitives
+--- PrimaryPart
+local function PrimaryPart(inst)
+	return Player.Optional.Character(inst) and Player.Optional.Character(inst).PrimaryPart;
+end
+Player.Optional.PrimaryPart = PrimaryPart;
+function Player.PrimaryPart(plrInstance)
+	local primaryPart = Player.Character(plrInstance).PrimaryPart;
+	if primaryPart then
+		return primaryPart;
+	end
+	Player.Character(plrInstance):WaitForChild("HumanoidRootPart", math.huge);
+	local primaryPart2 = Player.Character(plrInstance).PrimaryPart;
+	assert(primaryPart2);
+	return primaryPart2;
+end
+
+--- Humanoid primitive
+local function Humanoid(plrInstance)
+	return Player.Optional.Character(plrInstance) and Player.Optional.Character(plrInstance):FindFirstChildOfClass("Humanoid");
+end
+Player.Optional.Humanoid = Humanoid;
+function Player.Humanoid(plrInstance)
+	local plrHum = Player.Character(plrInstance):FindFirstChildOfClass("Humanoid");
+	if plrHum then
+		return plrHum;
+	end
+	Player.Character(plrInstance):WaitForChild("Humanoid", math.huge);
+	local hum2 = Player.Character(plrInstance):FindFirstChildOfClass("Humanoid");
+	assert(hum2);
+	return hum2;
+end
+
+--- Part primitive
+local function Part(plrInstance, partName)
+	return Player.Character(plrInstance):FindFirstChild(partName);
+end
+Player.Optional.Part = Part;
+function Player.Part(plrInstance, partName)
+	return Player.Character(plrInstance):WaitForChild(partName, math.huge);
+end
+
+--- EmbeddedPart primitive
+local function EmbeddedPart(plrInstance, partName, embedPartName)
+	return Player.Optional.Part(plrInstance, partName) and Player.Optional.Part(plrInstance, partName):FindFirstChild(embedPartName);
+end
+Player.Optional.EmbeddedPart = EmbeddedPart;
+function Player.EmbeddedPart(plrInstance, partName, embedPartName)
+	return Player.Part(plrInstance, partName):WaitForChild(embedPartName, math.huge);
+end
+
+--- Main player stuff. Don't modify, side effects include brain damage
+local function Head(inst) return Player.Optional.Part(inst, "Head") end
+function Player.Head(inst) return Player.Part(inst, "Head") end
+Player.Optional.Head = Head;
+local function UpperTorso(inst) return Player.Optional.Part(inst, "UpperTorso") end
+function Player.UpperTorso(inst) return Player.Part(inst, "UpperTorso") end
+Player.Optional.UpperTorso = UpperTorso;
+local function LowerTorso(inst) return Player.Optional.Part(inst, "LowerTorso") end
+function Player.LowerTorso(inst) return Player.Part(inst, "LowerTorso") end
+Player.Optional.LowerTorso = LowerTorso;
+local function LeftFoot(inst) return Player.Optional.Part(inst, "LeftFoot") end
+function Player.LeftFoot(inst) return Player.Part(inst, "LeftFoot") end
+Player.Optional.LeftFoot = LeftFoot;
+local function LeftHand(inst) return Player.Optional.Part(inst, "LeftHand") end
+function Player.LeftHand(inst) return Player.Part(inst, "LeftHand") end
+Player.Optional.LeftHand = LeftHand;
+local function LeftLowerArm(inst) return Player.Optional.Part(inst, "LeftLowerArm") end
+function Player.LeftLowerArm(inst) return Player.Part(inst, "LeftLowerArm") end
+Player.Optional.LeftLowerArm = LeftLowerArm
+local function LeftLowerLeg(inst) return Player.Optional.Part(inst, "LeftLowerLeg") end
+function Player.LeftLowerLeg(inst) return Player.Part(inst, "LeftLowerLeg") end
+Player.Optional.LeftLowerLeg = LeftLowerLeg;
+local function LeftUpperArm(inst) return Player.Optional.Part(inst, "LeftUpperArm") end
+function Player.LeftUpperArm(inst) return Player.Part(inst, "LeftUpperArm") end
+Player.Optional.LeftUpperArm = LeftUpperArm;
+local function LeftUpperLeg(inst) return Player.Optional.Part(inst, "LeftUpperLeg") end
+function Player.LeftUpperLeg(inst) return Player.Part(inst, "LeftUpperLeg") end
+Player.Optional.LeftUpperLeg = LeftUpperLeg;
+local function RightFoot(inst) return Player.Optional.Part(inst, "RightFoot") end
+function Player.RightFoot(inst) return Player.Part(inst, "RightFoot") end
+Player.Optional.RightFoot = RightFoot;
+local function RightHand(inst) return Player.Optional.Part(inst, "RightHand") end
+function Player.RightHand(inst) return Player.Part(inst, "RightHand") end
+Player.Optional.RightHand = RightHand;
+local function RightLowerArm(inst) return Player.Optional.Part(inst, "RightLowerArm") end
+function Player.RightLowerArm(inst) return Player.Part(inst, "RightLowerArm") end
+Player.Optional.RightLowerArm = RightLowerArm
+local function RightLowerLeg(inst) return Player.Optional.Part(inst, "RightLowerLeg") end
+function Player.RightLowerLeg(inst) return Player.Part(inst, "RightLowerLeg"); end
+Player.Optional.RightLowerLeg = RightLowerLeg;
+local function RightUpperArm(inst) return Player.Optional.Part(inst, "RightUpperArm"); end
+function Player.RightUpperArm(inst) return Player.Part(inst, "RightUpperArm") end
+Player.Optional.RightUpperArm = RightUpperArm;
+local function RightUpperLeg(inst) return Player.Optional.Part(inst, "RightUpperLeg") end
+function Player.RightUpperLeg(inst) return Player.Part(inst, "RightUpperLeg") end
+Player.Optional.RightUpperLeg = RightUpperLeg;
+
+--- Joint stuff
+local function Root(inst) return Player.Optional.EmbeddedPart(inst, "LowerTorso", "Root") end
+function Player.Root(inst) return Player.EmbeddedPart(inst, "LowerTorso", "Root") end
+Player.Optional.Root = Root;
+local function Waist(inst) return Player.Optional.EmbeddedPart(inst, "UpperTorso", "Waist") end
+function Player.Waist(inst) return Player.EmbeddedPart(inst, "UpperTorso", "Waist") end
+Player.Optional.Waist = Waist;
+local function Neck(inst) return Player.Optional.EmbeddedPart(inst, "Head", "Neck") end
+function Player.Neck(inst) return Player.EmbeddedPart(inst, "Head", "Neck") end
+Player.Optional.Neck = Neck;
+local function LeftAnkle(inst) return Player.Optional.EmbeddedPart(inst, "LeftFoot", "LeftAnkle") end
+function Player.LeftAnkle(inst) return Player.EmbeddedPart(inst, "LeftFoot", "LeftAnkle") end
+Player.Optional.LeftAnkle = LeftAnkle;
+local function LeftWrist(inst) return Player.Optional.EmbeddedPart(inst, "LeftHand", "LeftWrist") end
+function Player.LeftWrist(inst) return Player.EmbeddedPart(inst, "LeftHand", "LeftWrist") end
+Player.Optional.LeftWrist = LeftWrist;
+local function LeftElbow(inst) return Player.Optional.EmbeddedPart(inst, "LeftLowerArm", "LeftElbow") end
+Player.Optional.LeftElbow = LeftElbow;
+function Player.LeftElbow(inst) return Player.EmbeddedPart(inst, "LeftLowerArm", "LeftElbow") end
+local function LeftKnee(inst) return Player.Optional.EmbeddedPart(inst, "LeftLowerLeg", "LeftKnee") end
+function Player.LeftKnee(inst) return Player.EmbeddedPart(inst, "LeftLowerLeg", "LeftKnee") end
+Player.Optional.LeftKnee = LeftKnee;
+local function LeftShoulder(inst) return Player.Optional.EmbeddedPart(inst, "LeftUpperArm", "LeftShoulder") end
+function Player.LeftShoulder(inst) return Player.EmbeddedPart(inst, "LeftUpperArm", "LeftShoulder") end
+Player.Optional.LeftShoulder = LeftShoulder;
+local function LeftHip(inst) return Player.Optional.EmbeddedPart(inst, "LeftUpperLeg", "LeftHip") end
+function Player.LeftHip(inst) return Player.EmbeddedPart(inst, "LeftUpperLeg", "LeftHip") end
+Player.Optional.LeftHip = LeftHip;
+local function RightAnkle(inst) return Player.Optional.EmbeddedPart(inst, "RightFoot", "RightAnkle") end
+function Player.RightAnkle(inst) return Player.EmbeddedPart(inst, "RightFoot", "RightAnkle") end
+Player.Optional.RightAnkle = RightAnkle;
+local function RightWrist(inst) return Player.Optional.EmbeddedPart(inst, "RightHand", "RightWrist") end
+function Player.RightWrist(inst) return Player.EmbeddedPart(inst, "RightHand", "RightWrist") end
+Player.Optional.RightWrist = RightWrist;
+local function RightElbow(inst) return Player.Optional.EmbeddedPart(inst, "RightLowerArm", "RightElbow") end
+function Player.RightElbow(inst) return Player.EmbeddedPart(inst, "RightLowerArm", "RightElbow") end
+Player.Optional.RightElbow = RightElbow;
+local function RightKnee(inst) return Player.Optional.EmbeddedPart(inst, "RightLowerLeg", "RightKnee") end
+function Player.RightKnee(inst) return Player.EmbeddedPart(inst, "RightLowerLeg", "RightKnee") end
+Player.Optional.RightKnee = RightKnee;
+local function RightShoulder(inst) return Player.Optional.EmbeddedPart(inst, "RightUpperArm", "RightShoulder") end
+function Player.RightShoulder(inst) return Player.EmbeddedPart(inst, "RightUpperArm", "RightShoulder") end
+Player.Optional.RightShoulder = RightShoulder;
+local function RightHip(inst) return Player.Optional.EmbeddedPart(inst, "RightUpperLeg", "RightHip") end
+function Player.RightHip(inst) return Player.EmbeddedPart(inst, "RightUpperLeg", "RightHip") end
+Player.Optional.RightHip = RightHip;
+-- Player screen stuff
+local function PlayerGui(inst)
+	return Player.Player(inst):FindFirstChild("PlayerGui");
+end
+function Player.PlayerGui(inst)
+	return Player.Player(inst):WaitForChild("PlayerGui", math.huge);
+end
+Player.Optional.PlayerGui = PlayerGui;
+function Player.Mouse(inst)
+	return Player.Player(inst):GetMouse();
+end
+--- Cam
+function Player.Camera()
+	return workspace.CurrentCamera;
+end
+--- player name
+function Player.Name(inst)
+	return Player.Player(inst).Name;
+end
+--- display name
+function Player.DisplayName(inst)
+	return Player.Player(inst).DisplayName;
+end
+--- Animator
+local function Animator(inst)
+	local playerHumanoid = Player.Optional.Humanoid(inst);
+	if playerHumanoid then
+		local playerAnimator = playerHumanoid:FindFirstChildOfClass("Animator");
+		if playerAnimator then
+			return playerAnimator;
+		end
+		local plrAnimCtrl = playerHumanoid:FindFirstChildOfClass("AnimationController");
+		if plrAnimCtrl then
+			return plrAnimCtrl;
+		end
+		return playerHumanoid;
+	end
+	return nil;
+end
+Player.Optional.Animator = Animator;
+function Player.Animator(inst)
+	local playerHumanoid = Player.Humanoid(inst);
+	local playerAnimator = playerHumanoid:FindFirstChildOfClass("Animator");
+	if playerAnimator then
+		return playerAnimator;
+	end
+	local plrAnimCtrl = playerHumanoid:FindFirstChildOfClass("AnimationController");
+	if plrAnimCtrl then
+		return plrAnimCtrl;
+	end
+	return playerHumanoid;
+end
+
+return Player;

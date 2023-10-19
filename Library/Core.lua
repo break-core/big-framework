e75d76238174cbc65be46c13a363da96b2da6e95
Million1156 <justathrowermc3@gmail.com>
2023-10-19 14:09:08 -0500

    Add Library
    NOTE: Core is the actual Library script; due to how VSC/GitHub works, you can't nest Files inside of Files, so I just named it "Core" :/

---
+++ Library/Core.lua
@@ -0,0 +1,262 @@
+--[[
+	                                                                ,,╓╓╥╗╗@@@╣╢
+	                                               ,²╓╓╥╗@@@╣╣╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒U
+	                               ,,╓╓╥m╗@@@╣╢▒▒▒▒▒▒▒▒▒Ñ╜`         "╙╣▒▒▒▒▒▒▒▒╣
+	               ,,╓╓╗╗╗@@╣╣╢▒▒▒▒▒▒╣╝╨╜╙"`║▒▒▒▒▒▒▒▒╢╜                `╣▒▒▒▒▒▒▒
+	,╓╓╥╗@@@╣╢╢▒▒▒▒ÑÑ╝╝╨╜╨╝╝╣▒▒▒▒▒▒▒▒╢      ]▒▒▒▒▒▒▒╝         ,,         ╙▒▒▒▒▒▒
+	║▒▒▒▒▒"`                  `╝▒▒▒▒▒▒       ▒▒▒▒▒▒╝      ,@▒▒▒▒▒╣╗     ╓╗╣▒▒▒▒▒[
+	 ▒▒▒▒▒U                     └▒▒▒▒▒       ║▒▒▒▒╣       ╢▒▒▒▒▒▒▒▒╢╗╣╣▒▒▒▒▒▒▒▒▒▒
+	 ▒▒▒▒▒╣       ²╓╓╥╗╗╗╖       ]▒▒▒▒[      ]▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+	 ║▒▒▒▒▒       ▒▒▒▒▒▒▒▒╣      ]▒▒▒▒▒       ▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒╢╢Ñ╝╨╜╙╙▒▒▒▒▒[
+	 ]▒▒▒▒▒       ╢▒▒▒▒▒▒╢╜      ║▒▒▒▒▒       ╢▒▒▒[      ]▒▒▒▒▒[            ╢▒▒▒▒╢
+	  ▒▒▒▒▒[      └╙"``         @▒▒▒▒▒▒[      ]▒▒▒╢       ▒▒▒▒▒╣            ║▒▒▒▒▒
+	  ║▒▒▒▒╢                   ²╙╣▒▒▒▒▒▒       ▒▒▒▒       ║▒▒▒▒▒   ,²╓      ]▒▒▒▒▒U
+	  ]▒▒▒▒▒                       ╣▒▒▒▒       ╢▒▒▒╣       ╢▒▒▒▒▒▒▒▒▒╜       ▒▒▒▒▒╣
+	   ▒▒▒▒▒[      ]@╣╣▒▒▒▒╣╗       ▒▒▒▒[      ║▒▒▒▒╗       ╙╣▒▒▒▒▒Ñ`        ║▒▒▒▒▒
+	   ╢▒▒▒▒╢       ▒▒▒▒▒▒▒▒▒U      ║▒▒▒╢      ]▒▒▒▒▒@                       ]▒▒▒▒▒
+	   ║▒▒▒▒▒       ╣▒▒▒▒▒▒▒╢       ║▒▒▒▒       ▒▒▒▒▒▒╣╖               ,[     ▒▒▒▒▒[
+	    ▒▒▒▒▒U      ║Ñ╝╨╜╙"²        ▒▒▒▒▒       ║▒▒▒▒▒▒▒▒@╖,        ,╥╣▒╢@@╣╣╢▒▒▒▒╢╢
+	    ▒▒▒▒▒[                    ╓╢▒▒▒▒▒[      ]▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢╢▒╢ÑÑ╝╝╜╙""`
+	    ║▒▒▒▒▒                ,╓@╢▒▒▒▒▒▒▒▒@╣╣╢▒▒▒▒╢Ñ╝╝╨╜╙""`           ╓╗@@@╗╖
+	    ]▒▒▒▒▒    ,,╓╓╗╗@@@╣▒▒▒▒▒▒╢ÑÑ╝╝╜╙"``        ,²╓  ╔@@@╣╣╢▒▒▒U ,╣▒Ñ╙"╙╢▒Ñ
+	     ▒▒▒▒▒▒▒▒▒╢Ñ╝╝╨╜╙"``             ╥@@@╗    ]▒▒▒▒  ]▒▒╣"``     ]▒▒╗,
+	     "``       ,╓╖╓,       ]▒▒▒╣     ]▒▒▒▒╖   ║▒▒▒▒[  ▒▒▒   ,,    ╙╣▒▒▒▒╣%╖
+	            ,╣▒▒╢╣╣▒▒@     ╢▒▒╣▒╣     ▒▒╢╣▒╖  ▒▒[▒▒╢  ║▒▒▒▒▒▒▒r       `"╙╣▒▒╕
+	            ╣▒╣    ╙╜`     ▒▒╢ ╣▒╣    ▒▒▒╙▒▒╕]▒▒ ║▒▒  ]▒▒[         ╓@     ▒▒╢
+	           ║▒▒[       ,   ]▒▒[  ▒▒╣   ║▒▒ ╙▒▒@▒▒ ]▒▒L  ▒▒╢   ,,╓╓  ╣▒▒╣@╣▒▒╣
+	           ]▒▒[  ]╢▒▒▒▒[  ║▒▒╣╣╣╢▒▒╣  ]▒▒[ ╙▒▒▒[  ▒▒╣  ╢▒▒▒▒▒▒▒╢Ñ~    "╙"`  		
+	            ╢▒▒     ]▒▒╢  ╢▒▒╙"``╙▒▒@  ▒▒╢  ╙▒╢`  ╨╜╜  ``
+	             ╣▒▒h╖╥@▒▒▒▒ j▒▒╣     ╙╜╙` `
+	              `╙╝ÑÑ╝╙ "`
+                                                       
+    
+	BIG Games rbx.lua Framework [2017] - [2023]
+	Written by Preston - preston@biggames.io
+	Developed with a keyboard and pixie dust.
+	
+	-+-+-+- SPECIFICS -+-+-+-
+	_L
+	===========
+	Holds everything under Modules (and other stuff like Directory). The number before the module name represents the loading priority - very useful when a module
+	references another module. (how would you reference a module that hasan't loaded, yet?)
+	
+	Keep out of reach.
+	===========
+--]]
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- Settings
+local Debug = true
+
+--- Main
+local Library = {}
+
+--- References
+local isServer = game:GetService("RunService"):IsServer()
+local modules = game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Modules")
+local RunService = game:GetService("RunService")
+local Players = game:GetService("Players")
+
+--- Variables
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+--- Client specific stuff
+if not isServer then
+	--- Wait on intro to finish
+	repeat RunService.RenderStepped:Wait() until _G.IntroDone
+
+	--- Wait for GUIs to load (if intro is disabled)
+	Players.LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Message")
+end
+
+local startTick = tick()
+local Loaded = nil
+
+Library.Loaded = false
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--- Services (some scripts use "_L.FriendService" instead of "_L.Services.FriendService", for example)
+Library.Debris = game:GetService("Workspace"):WaitForChild("__DEBRIS")
+Library.Things = game:GetService("Workspace"):WaitForChild("__THINGS")
+Library.Assets = game:GetService("ReplicatedStorage"):WaitForChild("Assets")
+Library.AnalyticsService = game:GetService("AnalyticsService")
+Library.AssetService = game:GetService("AssetService")
+Library.BadgeService = game:GetService("BadgeService")
+Library.Chat =      game:GetService("Chat")
+Library.CollectionService = game:GetService("CollectionService")
+Library.ContentProvider =   game:GetService("ContentProvider")
+Library.ContextActionService = game:GetService("ContextActionService")
+Library.ControllerService = game:GetService("ControllerService")
+Library.DataStoreService =  game:GetService("DataStoreService")
+Library.FriendService =     game:GetService("FriendService")
+Library.GamepadService =    game:GetService("GamepadService")
+Library.GamePassService =   game:GetService("GamePassService")
+Library.GroupService = game:GetService("GroupService")
+Library.GuiService =   game:GetService("GuiService")
+Library.HapticService =     game:GetService("HapticService")
+Library.HttpService =  game:GetService("HttpService")
+Library.InsertService =     game:GetService("InsertService")
+Library.JointsService =     game:GetService("JointsService")
+Library.KeyboardService =   game:GetService("KeyboardService")
+Library.Lighting =  game:GetService("Lighting")
+Library.LocalizationService =  game:GetService("LocalizationService")
+Library.LoginService = game:GetService("LoginService")
+Library.LogService =   game:GetService("LogService")
+Library.MarketplaceService =   game:GetService("MarketplaceService")
+Library.MaterialService =   game:GetService("MaterialService")
+Library.MessagingService =  game:GetService("MessagingService")
+Library.MouseService = game:GetService("MouseService")
+Library.NotificationService =  game:GetService("NotificationService")
+Library.PathfindingService =   game:GetService("PathfindingService")
+Library.PermissionsService =   game:GetService("PermissionsService")
+Library.PhysicsService =    game:GetService("PhysicsService")
+Library.Players =   game:GetService("Players")
+Library.PointsService =     game:GetService("PointsService")
+Library.PolicyService =     game:GetService("PolicyService")
+Library.ProximityPromptService = game:GetService("ProximityPromptService")
+Library.ReplicatedFirst =   game:GetService("ReplicatedFirst")
+Library.ReplicatedStorage = game:GetService("ReplicatedStorage")
+Library.RunService = game:GetService("RunService")
+Library.SocialService = game:GetService("SocialService")
+Library.SoundService = game:GetService("SoundService")
+Library.StarterGui = game:GetService("StarterGui")
+Library.StarterPack = game:GetService("StarterPack")
+Library.StarterPlayer = game:GetService("StarterPlayer")
+Library.Teams = game:GetService("Teams")
+Library.TeleportService = game:GetService("TeleportService")
+Library.TextChatService = game:GetService("TextChatService")
+Library.TextService = game:GetService("TextService")
+Library.TouchInputService = game:GetService("TouchInputService")
+Library.TweenService = game:GetService("TweenService")
+Library.UserInputService = game:GetService("UserInputService")
+Library.UserService = game:GetService("UserService")
+Library.VoiceChatService = game:GetService("VoiceChatService")
+Library.VRService = game:GetService("VRService")
+Library.DebrisService = game:GetService("Debris")
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--- Different measurements of time
+Library.Heartbeat = function(Time)  
+	for i = 1, Time or 1 do
+		RunService.Heartbeat:Wait()
+	end
+end
+Library.Stepped = function(Time)
+	for i = 1, Time or 1 do
+		RunService.Stepped:Wait()
+	end
+end
+Library.RenderStepped = function(Time)  
+	for i = 1, Time or 1 do
+		RunService.RenderStepped:Wait()
+	end
+end
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+local UnaddedEntries = ""
+for _, module in ipairs(script:GetChildren()) do
+	if not module:IsA("ModuleScript") or Library[module.Name] ~= nil then 
+		continue 
+	end
+	UnaddedEntries = UnaddedEntries .. string.format("Library.%s = require(script.%s)\n", module.Name, module.Name)
+end
+if #UnaddedEntries > 0 and Debug then
+	warn(string.format("Unadded entries: \n\n%s\n", UnaddedEntries))
+end
+
+--- Check for entries
+for Modules, _ in pairs(Library) do
+	if script:FindFirstChild(Modules) then 
+		continue 
+	end
+	if Debug then
+		warn(string.format("Unknown entry: %s (this is safe to ignore)", tostring(Modules)))
+	end
+end
+for _, module in ipairs(script:GetChildren()) do
+	if not module:IsA("ModuleScript") or Library[module.Name] ~= nil then 
+		continue 
+	end
+	if module.Name == "Client" and RunService:IsServer() then
+		continue
+	end
+	
+	Library[module.Name] = require(module)
+end
+
+--- Only allow the server to access server resources
+if RunService:IsServer() then
+	Library.ServerScriptService =  game:GetService("ServerScriptService")
+	Library.ServerStorage =     game:GetService("ServerStorage")
+
+	local serverLibrary = require(game:GetService("ServerScriptService"):WaitForChild("Library"))
+	setmetatable(Library, {__index = serverLibrary})
+
+	Loaded = function()
+		return serverLibrary.Loaded
+	end
+else
+	local clientLibrary = require(script.Client)
+	setmetatable(Library, {__index = clientLibrary})
+
+	Loaded = function()
+		return clientLibrary.Loaded
+	end
+end
+
+task.spawn(function()
+	--- Load in modules
+	local finished = false
+	for i, child in ipairs(script:GetChildren()) do
+		if child.Name == "Client" or not child:IsA("ModuleScript") or Library[child.Name] ~= nil then continue end
+
+		--- Wait for modules to load
+		if not finished then
+			finished = true
+			task.wait()
+		end
+
+		--- Init module
+		Library[child.Name] = require(child)
+
+		--- Debug
+		print("Library (Shared) - Late loading: ", child)
+	end
+
+	while not Loaded() do
+		task.wait()
+	end
+
+	--- Done!
+	Library.Loaded = true
+
+	--- Debug
+	local initializing = RunService:IsClient() and "CLIENT" or "SERVER"
+	print(string.format("\226\156\133 %s | _L took %dms to initialize!", initializing, (math.round((tick() - startTick) * 1000))))
+end)
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+function Library.Load() 
+	while not Library.Loaded do
+		task.wait()
+	end
+end
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+return Library

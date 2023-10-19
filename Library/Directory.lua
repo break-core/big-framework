5092a05fcca0efcf1968889365cc972f6df3ada8
Million1156 <million@alyocord.com>
2023-10-19 13:09:19 -0500

    Finish Directory
    Finished Directory (checked for L4L, 95% accurate)
    
    Note that the function (starts_with) does not have accurate variable names, as we do not have the original ones (that's fine, I guess?)

--- Library/Directory.lua
+++ Library/Directory.lua
@@ -44,8 +44,7 @@
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-local debugInit = false
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 ----------------------
 
@@ -54,38 +53,42 @@ local Directory = {}
 ----------------------
 
 
+function starts_with(name, stringCheck)
+    if #name < #stringCheck then
+        return false
+    end
+    if stringCheck == "" then
+        return true
+    end
+    if name:sub(1, #stringCheck) == stringCheck then
+        return true
+    end
+    return false
+end
+
 --- Iterate through (physical) directory
-for _, dirFolder in ipairs(script:GetChildren()) do
-	--- Debug
-	if debugInit then  
-		print('-----------------------------------------------')  
-	end
-	
-	--- Setup directory
-	local dirName = dirFolder.Name
-	Directory[dirName] = {}
-	local dirData = Directory[dirName]
-	
-	--- Folder? Scan it!
-	for _, module in ipairs(dirFolder:GetDescendants()) do
-		if module.ClassName == "ModuleScript" then
-			--- Add module to directory
-			local moduleData = require(module)
-			for key, value in pairs(moduleData) do
-				dirData[key] = value
-				if debugInit then  print(key, value)  end
-			end
+local __DIRECTORY = ReplicatedStorage:WaitForChild("__DIRECTORY")
+for _, dirFolder in ipairs(__DIRECTORY:GetChildren()) do
+
+    --- Setup directory
+    local dirName = dirFolder.Name
+    Directory[dirName] = {}
+    local dirData = Directory[dirName]
+
+    --- Folder? Scan it!
+    for _, module in ipairs(dirFolder:GetChildren()) do
+        if module.ClassName ~= "ModuleScript" or module:GetAttribute("NOLOAD") then 
+		     continue 
 		end
-	end
-	
-	--- Debug
-	if debugInit then  
-		print('-----------------------------------------------\nLOADED '.. dirName)  
-	end
+        local moduleData = require(module)
+        for key, value in pairs(moduleData) do
+            dirData[key] = value
+        end
+    end
 end
 
 function Directory.NumberIDs(dirFolderName)
-	return #script[dirFolderName]:GetChildren()
+	return #ReplicatedStorage:WaitForChild("__DIRECTORY")[dirFolderName]:GetChildren()
 end
 
 ----------------------

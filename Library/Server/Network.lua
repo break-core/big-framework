0ded9fb569b312bed2c0494cfb0c6f68262329ab
Million1156 <justathrowermc3@gmail.com>
2023-10-19 22:50:36 -0500

    Server Network done!

--- Library/Server/Network.lua
+++ Library/Server/Network.lua
@@ -1,138 +1,300 @@
+--[[
+	                                                                ,,╓╓╥╗╗@@@╣╢
+	                                               ,²╓╓╥╗@@@╣╣╢▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒U
+	                               ,,╓╓╥m╗@@@╣╢▒▒▒▒▒▒▒▒▒Ñ╜`         "╙╣▒▒▒▒▒▒▒▒╣
+	               ,,╓╓╗╗╗@@╣╣╢▒▒▒▒▒▒╣╝╨╜╙"`║▒▒▒▒▒▒▒▒╢╜                `╣▒▒▒▒▒▒▒
+	,╓╓╥╗@@@╣╢╢▒▒▒▒ÑÑ╝╝╨╜╨╝╝╣▒▒▒▒▒▒▒▒╢      ]▒▒▒▒▒▒▒╝         ,,         ╙▒▒▒▒▒▒
+	║▒▒▒▒▒"`                  `╝▒▒▒▒▒▒       ▒▒▒▒▒▒╝      ,@▒▒▒▒▒╣╗     ╓╗╣▒▒▒▒▒[
+	 ▒▒▒▒▒U                     └▒▒▒▒▒       ║▒▒▒▒╣       ╢▒▒▒▒▒▒▒▒╢╗╣╣▒▒▒▒▒▒▒▒▒▒
+	 ▒▒▒▒▒╣       ²╓╓╥╗╗╗╖       ]▒▒▒▒[      ]▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+	 ║▒▒▒▒▒       ▒▒▒▒▒▒▒▒╣      ]▒▒▒▒▒       ▒▒▒▒[      ║▒▒▒▒▒▒▒▒▒▒╢╢Ñ╝╨╜╙╙▒▒▒▒▒[
+	 ]▒▒▒▒▒       ╢▒▒▒▒▒▒╢╜      ║▒▒▒▒▒       ╢▒▒▒[      ]▒▒▒▒▒[            ╢▒▒▒▒╢
+	  ▒▒▒▒▒[      └╙"``         @▒▒▒▒▒▒[      ]▒▒▒╢       ▒▒▒▒▒╣            ║▒▒▒▒▒
+	  ║▒▒▒▒╢                   ²╙╣▒▒▒▒▒▒       ▒▒▒▒       ║▒▒▒▒▒   ,²╓      ]▒▒▒▒▒U
+	  ]▒▒▒▒▒                       ╣▒▒▒▒       ╢▒▒▒╣       ╢▒▒▒▒▒▒▒▒▒╜       ▒▒▒▒▒╣
+	   ▒▒▒▒▒[      ]@╣╣▒▒▒▒╣╗       ▒▒▒▒[      ║▒▒▒▒╗       ╙╣▒▒▒▒▒Ñ`        ║▒▒▒▒▒
+	   ╢▒▒▒▒╢       ▒▒▒▒▒▒▒▒▒U      ║▒▒▒╢      ]▒▒▒▒▒@                       ]▒▒▒▒▒
+	   ║▒▒▒▒▒       ╣▒▒▒▒▒▒▒╢       ║▒▒▒▒       ▒▒▒▒▒▒╣╖               ,[     ▒▒▒▒▒[
+	    ▒▒▒▒▒U      ║Ñ╝╨╜╙"²        ▒▒▒▒▒       ║▒▒▒▒▒▒▒▒@╖,        ,╥╣▒╢@@╣╣╢▒▒▒▒╢╢
+	    ▒▒▒▒▒[                    ╓╢▒▒▒▒▒[      ]▒▒▒▒▒▒▒▒▒▒▒▒▒╢╢╢▒╢ÑÑ╝╝╜╙""`
+	    ║▒▒▒▒▒                ,╓@╢▒▒▒▒▒▒▒▒@╣╣╢▒▒▒▒╢Ñ╝╝╨╜╙""`           ╓╗@@@╗╖
+	    ]▒▒▒▒▒    ,,╓╓╗╗@@@╣▒▒▒▒▒▒╢ÑÑ╝╝╜╙"``        ,²╓  ╔@@@╣╣╢▒▒▒U ,╣▒Ñ╙"╙╢▒Ñ
+	     ▒▒▒▒▒▒▒▒▒╢Ñ╝╝╨╜╙"``             ╥@@@╗    ]▒▒▒▒  ]▒▒╣"``     ]▒▒╗,
+	     "``       ,╓╖╓,       ]▒▒▒╣     ]▒▒▒▒╖   ║▒▒▒▒[  ▒▒▒   ,,    ╙╣▒▒▒▒╣%╖
+	            ,╣▒▒╢╣╣▒▒@     ╢▒▒╣▒╣     ▒▒╢╣▒╖  ▒▒[▒▒╢  ║▒▒▒▒▒▒▒r       `"╙╣▒▒╕
+	            ╣▒╣    ╙╜`     ▒▒╢ ╣▒╣    ▒▒▒╙▒▒╕]▒▒ ║▒▒  ]▒▒[         ╓@     ▒▒╢
+	           ║▒▒[       ,   ]▒▒[  ▒▒╣   ║▒▒ ╙▒▒@▒▒ ]▒▒L  ▒▒╢   ,,╓╓  ╣▒▒╣@╣▒▒╣
+	           ]▒▒[  ]╢▒▒▒▒[  ║▒▒╣╣╣╢▒▒╣  ]▒▒[ ╙▒▒▒[  ▒▒╣  ╢▒▒▒▒▒▒▒╢Ñ~    "╙"`  		
+	            ╢▒▒     ]▒▒╢  ╢▒▒╙"``╙▒▒@  ▒▒╢  ╙▒╢`  ╨╜╜  ``
+	             ╣▒▒h╖╥@▒▒▒▒ j▒▒╣     ╙╜╙` `
+	              `╙╝ÑÑ╝╙ "`
+                                                       
+    
+	BIG Games rbx.lua Framework [2017] - [2023]
+	Written by Preston - preston@biggames.io
+	Developed with a keyboard and pixie dust.
+	
+	-+-+-+- SPECIFICS -+-+-+-
+	_L.Network
+	===========
+	Handles all the networking from client to server. Automatically compresses strings and tables to save bandwidth (theoretical)
+	Makes networking completely automatic and somehow also makes the syntax simplier. No fiddling with remotes required. B)
+	
+	Event example:
+		CLIENT:
+		_L.Network.Fire("Bullet Fired", bullet.CFrame, bullet, {speed = 33, damage = 423})
+		
+		SERVER:
+		_L.Network.Fired("Bullet Fired"):Connect(function(player, ...)
+			FireBullet(player, ...)
+		end)
+	
+	Function example:
+		CLIENT:
+		local success = _L.Network.Invoke("Buy", "Super Soaker 5000", 2455)
+		
+		SERVER:
+		_L.Network.Invoked("Buy").OnInvoke = function(player, itemName, price)
+			return (price <= 3000)
+		end
+		
+	===========
+		\\\ Subsititute for RemoteEvent:FireServer() / RemoteEvent:FireClient()
+		Network.Fire(
+			remoteName,				<--	|REQ|	Remote name											
+			player,					<-- |REQ|	Player instance	(SERVER ONLY)								
+			...,					<-- 		Anything :)							
+		)
+	
+		\\\ Subsititute for RemoteFunction:InvokeClient() / RemoteFunction:InvokeServer()
+		Network.Invoke(
+			remoteName,				<--	|REQ|	Remote name											
+			player,					<-- |REQ|	Player instance	(SERVER ONLY)								
+			...,					<-- 		Anything :)							
+		)
 
-local JobId = game.JobId
-if #JobId == 0 then
-	JobId = "00000000-0000-0000-0000-000000000000"
+		\\\ Subsititute for RemoteEvent.OnClientEvent / RemoteEvent.OnServerEvent
+		Network.Fired(
+			remoteName,				<--	|REQ|	Remote name			
+		)
+		
+		\\\ Subsititute for RemoteFunction.OnClientInvoke / RemoteFunction.OnServerInvoke
+		Network.Invoked.OnInvoke(
+			remoteName,				<--	|REQ|	Remote name			
+		)
+		
+		\\\ Subsititute for RemoteEvent:FireAllClients()
+		Network.FireAll(
+			remoteName,				<--	|REQ|	Remote name																		
+			...,					<-- 		Anything :)							
+		)
+--]]
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+--------|       Top       |--------
+local Network = {}
+
+--------|     Setting     |--------
+local compressionEnabled = true --- disable if you want to sacrifice bandwidth for server performance. Disabling defaults to vanilla remote behavior.
+
+--------|     Library     |--------
+local _L; coroutine.wrap(function()  _L = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))  end)()
+
+--------|    Reference    |--------
+local jobId = game.JobId
+if #jobId == 0 then
+	jobId = "00000000-0000-0000-0000-000000000000"
+end
+local placeholders = { "E", "F" }
+local networkData = { {}, {} }
+local remoteTypes = { "RemoteEvent", "RemoteFunction" }
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local sha1 = require(script:WaitForChild("SHA1"))
+
+--------|    Variables    |--------
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+local function generateNetworkKey(index, placeholder)
+	assert(typeof(index) == "number", "Invalid parameter type for index, expected number")
+	assert(typeof(placeholder) == "string", "Invalid parameter type for placeholder, expected string")
+
+	local placeholderData = networkData[index]
+	local key = placeholderData[placeholder]
+
+	if not key then
+		key = sha1("Network3//"..game.GameId.."/"..game.PlaceId.."/"..game.PlaceVersion.."/"..jobId.."/"..index.."/"..placeholder):reverse():sub(5, 36)
+		placeholderData[placeholder] = key
+	end
+
+	return key
 end
 
-local u1 = { {}, {} }
-local SHA1 = require(script.SHA1)
-local u3 = { {}, {} }
-local u4 = { "RemoteEvent", "RemoteFunction" }
-local l__ReplicatedStorage__5 = game:GetService("ReplicatedStorage")
-local function u6(p1, p2)
-	assert(typeof(p1) == "number")
-	assert(typeof(p2) == "string")
-	local v3 = u1[p1]
-	local v4 = v3[p2]
-	if not v4 then
-		v4 = SHA1("Network3/" .. "/" .. game.GameId .. "/" .. game.PlaceId .. "/" .. game.PlaceVersion .. "/" .. JobId .. "/" .. p1 .. "/" .. p2):reverse():sub(5, 36)
-		v3[p2] = v4
-	end
-	return v4
-end
-local v5 = {
-	Fire = function(p3, p4, ...)
-		local v6 = false
-		if typeof(p4) == "Instance" then
-			v6 = p4:IsA("Player")
-		end
-		assert(v6)
-		if not p4:FindFirstChild("__LOADED") then
-			return
-		end
-		local v7 = u6(1, p3)
-		local v8 = u3[1]
-		local v9 = v8[v7]
-		if not v9 then
-			v9 = Instance.new(u4[1])
-			v9.Name = v7
-			v9.Parent = l__ReplicatedStorage__5
-			v8[v7] = v9
-		end
-		task.spawn(function(...)
-			v9:FireClient(p4, ...)
-		end, ...)
-	end
-}
-local l__Players__7 = game:GetService("Players")
-function v5.FireAll(p5, ...)
-	local v10 = l__Players__7:GetPlayers()
-	local v11 = {}
-	for v12, v13 in ipairs(v10) do
-		if v13:FindFirstChild("__LOADED") then
-			table.insert(v11, v13)
-		end	
-	end
-	local v15 = u6(1, p5)
-	local v16 = u3[1]
-	local v17 = v16[v15]
-	if not v17 then
-		v17 = Instance.new(u4[1])
-		v17.Name = v15
-		v17.Parent = l__ReplicatedStorage__5
-		v16[v15] = v17
-	end
-	if #v11 <= #v10 then
-		task.spawn(function(...)
-			v17:FireAllClients(...)
-		end, ...)
+--- Fire
+Network.Fire = function(placeholder, player, ...)
+	assert(typeof(player) == "Instance" and player:IsA("Player"), "Invalid parameter type for player, expected Player")
+
+	if not player:FindFirstChild("__LOADED") then
 		return
 	end
-	for v18, v19 in ipairs(v11) do
+	
+	--- Get/create remote
+	local networkKey = generateNetworkKey(1, placeholder)
+	local remoteData = networkData[1]
+	local remoteObject = remoteData[networkKey]
+
+	--- Create packet with arguments
+	if not remoteObject then
+		-- Create a new RemoteEvent if it doesn't exist
+		remoteObject = Instance.new(remoteTypes[1])
+		remoteObject.Name = networkKey
+		remoteObject.Parent = replicatedStorage
+		remoteData[networkKey] = remoteObject
+	end
+
+	--- Send
+	task.spawn(function(...)
+		remoteObject:FireClient(player, ...)
+	end, ...)
+end
+
+--- Fire (everyone)
+Network.FireAll = function(placeholder, ...)
+
+	--- Get list of players loaded past intro (that we can fire event on)
+	local players = game:GetService("Players"):GetPlayers()
+	local playersToSend = {}
+	--
+	for _, player in ipairs(players) do
+		if player:FindFirstChild("__LOADED") then
+			table.insert(playersToSend, player)
+		end
+	end
+
+	--- Get/create remote
+	local networkKey = generateNetworkKey(1, placeholder)
+	local remoteData = networkData[1]
+	local remoteObject = remoteData[networkKey]
+
+	--- Create packet with arguments
+	if not remoteObject then
+		remoteObject = Instance.new(remoteTypes[1])
+		remoteObject.Name = networkKey
+		remoteObject.Parent = replicatedStorage
+		remoteData[networkKey] = remoteObject
+	end
+
+	--- Send
+	if #playersToSend <= #players then
 		task.spawn(function(...)
-			v17:FireClient(v19, ...)
-		end, ...)	
-	end
-end
-function v5.Fired(p6)
-	local v21 = u6(1, p6)
-	local v22 = u3[1]
-	local v23 = v22[v21]
-	if not v23 then
-		v23 = Instance.new(u4[1])
-		v23.Name = v21
-		v23.Parent = l__ReplicatedStorage__5
-		v22[v21] = v23
-	end
-	return v23.OnServerEvent
-end
-function v5.Invoke(p7, p8, ...)
-	local v24 = false
-	if typeof(p8) == "Instance" then
-		v24 = p8:IsA("Player")
-	end
-	assert(v24)
-	if not p8:FindFirstChild("__LOADED") then
+			--- Can use optimized FireAllClients
+			remoteObject:FireAllClients(...)
+		end, ...)
+
 		return
 	end
-	local v25 = u6(2, p7)
-	local v26 = u3[2]
-	local v27 = v26[v25]
-	if not v27 then
-		v27 = Instance.new(u4[2])
-		v27.Name = v25
-		v27.Parent = l__ReplicatedStorage__5
-		v26[v25] = v27
+
+	--- Some players are in intro, so we gotta fire each manually
+	for _, player in ipairs(playersToSend) do
+		task.spawn(function(...)
+			remoteObject:FireClient(player, ...)
+		end, ...)
 	end
-	return v27:InvokeClient(p8, ...)
 end
-function v5.Invoked(p9)
-	local v28 = u6(2, p9)
-	local v29 = u3[2]
-	local v30 = v29[v28]
-	if not v30 then
-		v30 = Instance.new(u4[2])
-		v30.Name = v28
-		v30.Parent = l__ReplicatedStorage__5
-		v29[v28] = v30
+
+--- On Fire
+Network.Fired = function(placeholder)
+
+	--- Variables
+	local networkKey = generateNetworkKey(1, placeholder)
+	local remoteData = networkData[1]
+	local remoteObject = remoteData[networkKey]
+
+	--- Get/create bindableEvent
+	if not remoteObject then
+		remoteObject = Instance.new(remoteTypes[1])
+		remoteObject.Name = networkKey
+		remoteObject.Parent = replicatedStorage
+		remoteData[networkKey] = remoteObject
 	end
-	local v31 = {}
-	function v31.__newindex(p10, p11, p12)
-		if p11 == "OnInvoke" then
-			p11 = "OnServerInvoke"
-		elseif p11 == "OnServerInvoke" then
-			error(string.format("%s is not a valid member of BindableFunction \"BindableFunction\"", tostring(p11)))
-		end
-		v30[p11] = p12
-	end
-	function v31.__index(p13, p14)
-		if p14 == "OnInvoke" then
-			p14 = "OnServerInvoke"
-		elseif p14 == "OnServerInvoke" then
-			error(string.format("%s is not a valid member of BindableFunction \"BindableFunction\"", tostring(p14)))
-		end
-		return v30[p14]
-	end
-	return setmetatable({}, v31)
+
+	--
+	return remoteObject.OnServerEvent
 end
-return v5
+
+--- Invoke
+Network.Invoke = function(placeholder, player, ...)
+	assert(typeof(player) == "Instance" and player:IsA("Player"), "Invalid parameter type for player, expected Player")
+
+	--- Check if player is past intro
+	if not player:FindFirstChild("__LOADED") then
+		return
+	end
+
+	--- Get/create remote
+	local networkKey = generateNetworkKey(2, placeholder)
+	local remoteData = networkData[2]
+	local remoteObject = remoteData[networkKey]
+
+	--- Create packet with arguments
+	if not remoteObject then
+ 		remoteObject = Instance.new(remoteTypes[2])
+		remoteObject.Name = networkKey
+		remoteObject.Parent = replicatedStorage
+		remoteData[networkKey] = remoteObject
+	end
+
+	--- Return decoded packet
+	return remoteObject:InvokeClient(player, ...)
+end
+
+--- On Invoke
+Network.Invoked = function(placeholder)
+
+	--- Get/create bindableFunction
+	local networkKey = generateNetworkKey(2, placeholder)
+	local remoteData = networkData[2]
+	local remoteObject = remoteData[networkKey]
+
+	--- Create packet with arguments
+	if not remoteObject then
+		remoteObject = Instance.new(remoteTypes[2])
+		remoteObject.Name = networkKey
+		remoteObject.Parent = replicatedStorage
+		remoteData[networkKey] = remoteObject
+	end
+
+	--- Create a metatable to handle setting and getting function handlers
+	local metatable = {}
+
+	function metatable.__newindex(_, member, value)
+		if member == "OnInvoke" then
+			member = "OnServerInvoke"
+		elseif member == "OnServerInvoke" then
+			error(string.format("%s is not a valid member of BindableFunction \"BindableFunction\"", tostring(member)))
+		end
+
+		remoteObject[member] = value
+	end
+
+	function metatable.__index(_, member)
+		if member == "OnInvoke" then
+			member = "OnServerInvoke"
+		elseif member == "OnServerInvoke" then
+			error(string.format("%s is not a valid member of BindableFunction \"BindableFunction\"", tostring(member)))
+		end
+
+		return remoteObject[member]
+	end
+
+	return setmetatable({}, metatable)
+end
+return Network
